332b27236259815ab13d87055bb8673ee938d69e
harish8118 <harishkonchadaios@gmail.com>
2020-12-12 15:16:31 +0530

    Initial Commit

---
+++ MG Student/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MG Student
+//
+//  Created by Cyberheights Software Technologies Pvt Ltd on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
